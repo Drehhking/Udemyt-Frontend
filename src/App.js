bfe61e24d56c676ce649e73e6d18fe62c198cd47
Drehhking <ahmedrichard208@gmail.com>
2024-12-19 02:33:44 +0100

    nyyoht

--- src/App.js
+++ src/App.js
@@ -62,7 +62,7 @@ const App = () => {
           <Route path="/coursean/categoryList" element={<CategoryPage/>} />
           {/* <Route path="/edit" element={<Edit/>} /> */}
           <Route path="/image" element={<Image/>} />
-          <Route path="/purchase" element={<Purchase/>} />
+          <Route path="/purchased-courses" element={<Purchase/>} />
         </Routes>
       </Router>
       <Sidebar />
